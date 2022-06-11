f885efcfba1b6b8d0216d64e5b8d2146eeec4644
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-11 23:38:30 +0700

    feat: routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { PrimaryAlert } from "./components/alert";
+import BackButton from "./components/button/BackButton";
+import PrimaryButton from "./components/button/PrimaryButton";
+import SecondaryButton from "./components/button/SecondaryButton";
+import LabelOptionInput from "./components/input/LabelOptionInput";
+import LabelPasswordInput from "./components/input/LabelPasswordInput";
+import LabelTextareaInput from "./components/input/LabelTextareaInput";
+import LabelTextInput from "./components/input/LabelTextInput";
+import SearchInput from "./components/input/SearchInput";
+import RoutesApp from "./routes";
 
 function App() {
-  return (
-    <div className="w-screen h-screen grid place-items-center px-[135px]">
-      <PrimaryAlert>Status produk berhasil diperbarui</PrimaryAlert>
-    </div>
-  );
+  return <RoutesApp />;
 }
 
 export default App;
---
+++ src/pages/Components.js
@@ -0,0 +1,43 @@
+import { PrimaryAlert } from "../components/alert";
+import BackButton from "../components/button/BackButton";
+import PrimaryButton from "../components/button/PrimaryButton";
+import SecondaryButton from "../components/button/SecondaryButton";
+import LabelOptionInput from "../components/input/LabelOptionInput";
+import LabelPasswordInput from "../components/input/LabelPasswordInput";
+import LabelTextareaInput from "../components/input/LabelTextareaInput";
+import LabelTextInput from "../components/input/LabelTextInput";
+import SearchInput from "../components/input/SearchInput";
+
+export default function Components() {
+  return (
+    <div className="w-screen min-h-screen py-8 px-[135px] flex flex-col space-y-6">
+      <PrimaryButton>PrimaryButton</PrimaryButton>
+      <PrimaryButton isSmall>PrimaryButton small</PrimaryButton>
+      <PrimaryButton isDisable>PrimaryButton disabled</PrimaryButton>
+      <PrimaryButton isSmall isDisable>
+        PrimaryButton small disabled
+      </PrimaryButton>
+      <SecondaryButton>SecondaryButton</SecondaryButton>
+      <SecondaryButton isSmall>SecondaryButton small</SecondaryButton>
+      <SecondaryButton isDisable>SecondaryButton disabled</SecondaryButton>
+      <SecondaryButton isSmall isDisable>
+        SecondaryButton small disabled
+      </SecondaryButton>
+      <BackButton />
+      <PrimaryAlert bgColor="bg-alert-success">
+        PrimaryAlert bg-alert-success
+      </PrimaryAlert>
+      <PrimaryAlert bgColor="bg-alert-danger">
+        PrimaryAlert bg-alert-danger
+      </PrimaryAlert>
+      <PrimaryAlert bgColor="bg-alert-warning">
+        PrimaryAlert bg-alert-warning
+      </PrimaryAlert>
+      <LabelTextInput placeholder="LabelTextInput" />
+      <LabelPasswordInput placeholder="LabelPasswordInput" />
+      <LabelTextareaInput placeholder="LabelTexareaInput" />
+      <LabelOptionInput placeholder="LabelOptionInput" />
+      <SearchInput placeholder="SearchInput" />
+    </div>
+  );
+}
