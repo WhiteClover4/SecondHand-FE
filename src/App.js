81a5ad21e54b606659c8278e4f684051cf411703
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-14 19:39:24 +0700

    remove: unused code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import { PrimaryAlert } from "./components/alert";
-import BackButton from "./components/button/BackButton";
-import PrimaryButton from "./components/button/PrimaryButton";
-import SecondaryButton from "./components/button/SecondaryButton";
-import LabelOptionInput from "./components/input/LabelOptionInput";
-import LabelPasswordInput from "./components/input/LabelPasswordInput";
-import LabelTextareaInput from "./components/input/LabelTextareaInput";
-import LabelTextInput from "./components/input/LabelTextInput";
-import SearchInput from "./components/input/SearchInput";
 import RoutesApp from "./routes";
 
 function App() {
