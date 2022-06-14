a26898f98a7daa3bed630a7ea38f55eebcdc3286
Syahriful <dynes2014@gmail.com>
2022-06-14 20:51:55 +0700

    menambahkan feat/card

---
+++ src/components/card/Card.js
@@ -0,0 +1,10 @@
+export default function Card() {
+  return (
+    <div className="w-full rounded p-2 pb-4 shadow-low">
+      <img src="/img/Card-Image.png" alt="CardImage" className="h-[100px] w-full rounded object-cover" />
+      <p className="text-body-14">Jam Tangan Casio</p>
+      <p className="text-body-10 text-neutral-03">Aksesoris</p>
+      <p className="text-body-14">RP 250.000</p>
+    </div>
+  );
+}
