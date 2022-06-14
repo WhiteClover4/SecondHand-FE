4109e57e53b13a9f494a343182d190d2f8cffde0
Syahriful <dynes2014@gmail.com>
2022-06-14 21:06:10 +0700

    menambahkan margin untuk tiap item card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,9 +2,9 @@ export default function Card() {
   return (
     <div className="w-full rounded p-2 pb-4 shadow-low">
       <img src="/img/Card-Image.png" alt="CardImage" className="h-[100px] w-full rounded object-cover" />
-      <p className="text-body-14">Jam Tangan Casio</p>
-      <p className="text-body-10 text-neutral-03">Aksesoris</p>
-      <p className="text-body-14">RP 250.000</p>
+      <p className="mt-2 text-body-14">Jam Tangan Casio</p>
+      <p className="mt-1 text-body-10 text-neutral-03">Aksesoris</p>
+      <p className="mt-2 text-body-14">RP 250.000</p>
     </div>
   );
 }
