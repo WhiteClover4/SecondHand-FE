36fd8ea7bf8901e6cab885862f2c41e9fb35aa9b
sitiarifah <arfharifah@gmail.com>
2022-06-17 23:21:41 +0800

    profil card

--- src/components/card/ProfilCard.js
+++ src/components/card/ProfilCard.js
@@ -3,7 +3,7 @@ export default function ProfilCard() {
         <div className="w-[336px] h-20 flex flex-row  p-4 rounded-xl shadow-low bg-neutral-01">
             <img src="/img/profilcard.png" alt="profilcard" className="h-12 w-12 rounded object-cover" />
             <div className=" ml-4 mt=[21px] text-body-14"> Nama Penjual
-                <p className=" mt-1 mb-[21px] text-body-10 text-neutral-03 space-y-1 ">Kota</p>
+                <p className=" mt-1 mb-[21px] space-y-1 text-body-10 text-neutral-03 ">Kota</p>
             </div>
         </div >
     );
