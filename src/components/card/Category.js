51b54a26ba574521c68e8201474daed996adba70
Syahriful <dynes2014@gmail.com>
2022-06-21 19:39:57 +0700

    adding color for text and fixing svg attributes

--- src/components/card/Category.js
+++ src/components/card/Category.js
@@ -7,21 +7,21 @@ export default function Category() {
       <div className="flex flex-col gap-4">
         <button type="button" className="group flex gap-2 border-b-[1px] border-neutral-02 pb-4">
           <BoxIcon className="h-6 w-6 text-neutral-03 group-hover:text-primary-04" />
-          <p className="w-[156px] text-start text-title-16 group-hover:font-medium group-hover:text-primary-04">
+          <p className="w-[156px] text-start text-title-16 text-neutral-05 group-hover:font-medium group-hover:text-primary-04">
             Semua Produk
           </p>
           <ChevronRightIcon className="h-6 w-6 text-neutral-02 group-hover:text-primary-04" />
         </button>
         <button type="button" className="group flex gap-2 border-b-[1px] border-neutral-02 pb-4">
           <HeartIcon className="h-6 w-6 text-neutral-03 group-hover:text-primary-04" />
-          <p className="w-[156px] text-start text-title-16 group-hover:font-medium group-hover:text-primary-04">
+          <p className="w-[156px] text-start text-title-16 text-neutral-05 group-hover:font-medium group-hover:text-primary-04">
             Diminati
           </p>
           <ChevronRightIcon className="h-6 w-6 text-neutral-02 group-hover:text-primary-04" />
         </button>
         <button type="button" className="group flex gap-2 border-neutral-02 pb-4">
           <DollarSignIcon className="h-6 w-6 text-neutral-03 group-hover:text-primary-04" />
-          <p className="w-[156px] text-start text-title-16 group-hover:font-medium group-hover:text-primary-04">
+          <p className="w-[156px] text-start text-title-16 text-neutral-05 group-hover:font-medium group-hover:text-primary-04">
             Terjual
           </p>
           <ChevronRightIcon className="h-6 w-6 text-neutral-02 group-hover:text-primary-04" />
--- src/components/icon/ChevronRightIcon.js
+++ src/components/icon/ChevronRightIcon.js
@@ -1,7 +1,13 @@
 export default function ChevronRightIcon({ className }) {
   return (
     <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path
+        d="M9 18L15 12L9 6"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 }
