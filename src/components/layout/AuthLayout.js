c3edd84d466e380f863f2f56a2efad991288d7c1
Syahriful <dynes2014@gmail.com>
2022-06-21 23:33:15 +0700

    revisi

--- src/components/layout/AuthLayout.js
+++ src/components/layout/AuthLayout.js
@@ -1,9 +1,15 @@
 export default function AuthLayout({ children }) {
   return (
     <div className="flex h-screen w-screen">
-      <div className="h-full w-1/2 bg-red-300"></div>
-      <div className="flex h-full w-1/2 items-center bg-blue-300 px-[134px]">
-        <div className="bg-green-300 w-full">{children}</div>
+      <div className="relative h-full w-1/2 bg-slate-600">
+        <p className="absolute top-1/2 left-20 w-[154px] -translate-y-1/2 transform text-[40px] font-bold leading-9 text-white">
+          Second Hand.
+        </p>
+        <div className="absolute h-full w-full bg-gradient-to-t from-[#A06ECE] opacity-50"></div>
+        <img src="/img/img-bg-auth.png" className="h-full w-full object-cover" />
+      </div>
+      <div className="flex h-full w-1/2 items-center px-[134px]">
+        <div className="flex w-full flex-col items-center">{children}</div>
       </div>
     </div>
   );
