53473115e978d85eded6de9647038d1107c4c2b4
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-21 19:23:45 +0700

    fix: profilecard

--- src/pages/Components.js
+++ src/pages/Components.js
@@ -1,40 +1,22 @@
 import { PrimaryAlert } from "../components/alert";
-import BackButton from "../components/button/BackButton";
-import InputButton from "../components/button/InputButton";
-import PrimaryButton from "../components/button/PrimaryButton";
-import SecondaryButton from "../components/button/SecondaryButton";
-import LabelOptionInput from "../components/input/LabelOptionInput";
-import LabelPasswordInput from "../components/input/LabelPasswordInput";
-import LabelTextareaInput from "../components/input/LabelTextareaInput";
-import LabelTextInput from "../components/input/LabelTextInput";
-import SearchInput from "../components/input/SearchInput";
-import ProfilCard from "../components/card/ProfilCard";
-/* import {
+import {
   FileInput,
+  FileInput2,
+  FileInput3,
   LabelOptionInput,
   LabelPasswordInput,
   LabelTextareaInput,
   LabelTextInput,
   SearchInput,
 } from "../components/input";
-
 import {
   PrimaryButton,
   SecondaryButton,
   BackButton,
 } from "../components/button";
-import { PrimaryButton, SecondaryButton, BackButton } from "../components/button";
-*/
 
-import {
-  FileInput,
-  FileInput2,
-  } from "../components/input";
-
-
-import { Card, Card2 } from "../components/card";
+import { Card, Card2, ProfilCard } from "../components/card";
 import { SuccessCheckoutModal } from "../components/modal";
-
 
 export default function Components() {
   return (
@@ -52,23 +34,40 @@ export default function Components() {
         SecondaryButton small disabled
       </SecondaryButton>
       <BackButton />
-      <PrimaryAlert bgColor="bg-alert-success">PrimaryAlert bg-alert-success</PrimaryAlert>
-      <PrimaryAlert bgColor="bg-alert-danger">PrimaryAlert bg-alert-danger</PrimaryAlert>
-      <PrimaryAlert bgColor="bg-alert-warning">PrimaryAlert bg-alert-warning</PrimaryAlert>
+      <PrimaryAlert bgColor="bg-alert-success">
+        PrimaryAlert bg-alert-success
+      </PrimaryAlert>
+      <PrimaryAlert bgColor="bg-alert-danger">
+        PrimaryAlert bg-alert-danger
+      </PrimaryAlert>
+      <PrimaryAlert bgColor="bg-alert-warning">
+        PrimaryAlert bg-alert-warning
+      </PrimaryAlert>
       <LabelTextInput label="LabelTextInput" placeholder="LabelTextInput" />
-      <LabelPasswordInput label="LabelPasswordInput" placeholder="LabelPasswordInput" />
-      <LabelTextareaInput label="LabelTexareaInput" placeholder="LabelTexareaInput" />
-      <LabelOptionInput label="LabelOptionInput" placeholder="LabelOptionInput" />
+      <LabelPasswordInput
+        label="LabelPasswordInput"
+        placeholder="LabelPasswordInput"
+      />
+      <LabelTextareaInput
+        label="LabelTexareaInput"
+        placeholder="LabelTexareaInput"
+      />
+      <LabelOptionInput
+        label="LabelOptionInput"
+        placeholder="LabelOptionInput"
+      />
       <SearchInput placeholder="SearchInput" />
       <div className="grid grid-cols-4">
         {" "}
-        <FileInput />
-        {" "}
+        <FileInput />{" "}
       </div>
       <div>
         <FileInput />
+      </div>
+      <div>
         <FileInput2 />
       </div>
+      <FileInput3 />
       <div className="grid grid-cols-6 gap-4 ">
         <Card />
         <Card />
@@ -83,9 +82,6 @@ export default function Components() {
         <Card2 />
       </div>
       <SuccessCheckoutModal />
-      <div>
-        <InputButton />
-      </div>
       <div>
         <ProfilCard />
       </div>
