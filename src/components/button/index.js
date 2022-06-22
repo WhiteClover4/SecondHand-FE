a3528924fdf0e6e5f7e21c31735e4d58b5ec2a0d
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-22 19:34:14 +0700

    remove: unused file

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,5 @@
 import PrimaryButton from "./PrimaryButton";
 import SecondaryButton from "./SecondaryButton";
 import BackButton from "./BackButton";
-import AddProductInput from "./AddProductInput";
 
-export { PrimaryButton, SecondaryButton, BackButton, AddProductInput };
+export { PrimaryButton, SecondaryButton, BackButton };
