f98adbc4a2d46a64d9d888d30eb954d430f57574
sitiarifah <arfharifah@gmail.com>
2022-06-22 22:30:48 +0800

    Halaman Login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Route, Routes as Switch } from "react-router-dom";
 import Components from "../pages/Components";
+import Login from "../pages/Login";
 
 export default function RoutesApp() {
   return (
     <Switch>
       {/* <Route path="/" element={<Components />} /> */}
       <Route path="/components" element={<Components />} />
+      <Route path="/Login" element={<Login />} />
     </Switch>
   );
 }
