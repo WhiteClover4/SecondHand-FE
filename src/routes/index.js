7601d7c53bdba15bcd7ffb08bee899859afe1587
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-26 21:45:17 +0700

    feat: route to seller/products

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Components from '../pages/Components';
 import PageAuthLayout from '../pages/PageAuthLayout';
 import Home from '../pages/Home';
 import SoldSeller from '../pages/seller/Sold';
+import ProductsSeller from '../pages/seller/Products';
 
 export default function RoutesApp() {
   return (
@@ -11,6 +12,7 @@ export default function RoutesApp() {
       <Route element={<Components />} path="/components" />
       <Route element={<PageAuthLayout />} path="/page-auth" />
       <Route element={<SoldSeller />} path="/seller/sold" />
+      <Route element={<ProductsSeller />} path="/seller/products" />
     </Switch>
   );
 }
