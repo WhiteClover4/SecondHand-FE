16eaf389e506741b8a00e1bb4db700a9d92d17e3
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-26 22:21:15 +0700

    feat: route to product name

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Route, Routes as Switch } from 'react-router-dom';
 import Components from '../pages/Components';
 import PageAuthLayout from '../pages/PageAuthLayout';
 import Home from '../pages/Home';
-import Product from '../pages/Product';
+import Product from '../pages/[productName]';
 import InterestedSeller from '../pages/seller/Interested';
 import SoldSeller from '../pages/seller/Sold';
 import ProductsSeller from '../pages/seller/Products';
@@ -11,7 +11,7 @@ export default function RoutesApp() {
   return (
     <Switch>
       <Route element={<Home />} path="/" />
-      <Route element={<Product />} path="/product" />
+      <Route element={<Product />} path="/product/:productName" />
       <Route element={<Components />} path="/components" />
       <Route element={<PageAuthLayout />} path="/page-auth" />
       <Route element={<InterestedSeller />} path="/seller/interested" />
