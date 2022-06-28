05d24aa57d2cc901c7b20a51339bc31d8d690cb1
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-28 12:56:00 +0700

    feat: goback

--- src/components/buttons/BackButton.js
+++ src/components/buttons/BackButton.js
@@ -1,8 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import { ArrowLeftIcon } from '../icons';
 
 export default function BackButton({ ...rest }) {
+  const navigate = useNavigate();
   return (
-    <button {...rest}>
+    <button {...rest} onClick={() => navigate(-1)} type="button">
       <ArrowLeftIcon className="h-6 w-6 text-neutral-05" />
     </button>
   );
