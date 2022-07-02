9e25e863f544c11fc6f351c75bb3a51e1b0a461f
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-07-02 19:57:15 +0700

    remove: unused redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slice/auth';
 import alertReducer from './slice/alert';
-import productReducer from './slice/product';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     alert: alertReducer,
-    product: productReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
 });
