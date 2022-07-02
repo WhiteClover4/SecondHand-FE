50cf4a3b17a9325b771cdaf981bc48bcecb89467
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-07-03 00:01:52 +0700

    fix: file input 3

--- src/components/inputs/FileInput3.js
+++ src/components/inputs/FileInput3.js
@@ -1,7 +1,15 @@
+import { CameraIcon } from '../icons';
+
 export default function FileInput3({ preview, onChange }) {
   return (
     <button className="relative h-24 w-24 overflow-hidden rounded-xl">
-      <img className="h-full w-full object-contain" src={preview} />
+      {preview ? (
+        <img className="h-full w-full object-contain" src={preview} />
+      ) : (
+        <div className="flex h-full w-full items-center justify-center bg-[#E2D4F0]">
+          <CameraIcon className="h-6 w-6 text-primary-04" />
+        </div>
+      )}
       <input
         accept="image/*"
         className="absolute top-0 left-0 h-full w-full opacity-0"
