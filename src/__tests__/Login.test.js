046456489a4b2cc3b0cdad31049150e54d65dfec
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-07-06 00:53:54 +0700

    feat: login tests

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -21,16 +21,74 @@ describe('Login', () => {
     render(<LoginComp />);
   });
 
-  it('success', async () => {
+  function mockFetchAPI(email, password, isServerDown) {
+    if (isServerDown) return fetch.mockReject(() => Promise.reject('505'));
+
+    let mockResponse = {};
+    const correctEmail = 'correct@mail.com';
+    const correctPassword = '12345678';
+
+    if (email !== correctEmail) {
+      mockResponse = { status: 'Failed', msg: 'Wrong email or password' };
+    } else if (password !== correctPassword) {
+      mockResponse = { status: 'Failed', msg: 'Wrong email or password' };
+      return;
+    } else if (email === correctEmail && password === correctPassword) {
+      mockResponse = { token: '123', msg: 'Login success' };
+    }
+
+    fetch.mockResponseOnce(JSON.stringify(mockResponse));
+  }
+
+  it('render fail alert internal server error', async () => {
+    const loginButton = screen.getByTestId('login-button');
+
+    act(() => {
+      mockFetchAPI(undefined, undefined, true);
+
+      userEvent.click(loginButton);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('something went wrong')).toBeInTheDocument();
+    });
+  });
+
+  it('render fail alert invalid credentials', async () => {
     const emailInput = screen.getByText('Email');
     const passwordInput = screen.getByText('Password');
     const loginButton = screen.getByTestId('login-button');
 
-    act(() => {
-      userEvent.type(emailInput, 'correct@mail.com');
-      userEvent.type(passwordInput, '12345678');
+    const emailType = 'wrong@mail.com';
+    const passwordType = '12345678';
 
-      fetch.mockResponseOnce(JSON.stringify({ token: '123', msg: 'Login success' }));
+    act(() => {
+      userEvent.type(emailInput, emailType);
+      userEvent.type(passwordInput, passwordType);
+
+      mockFetchAPI(emailType, passwordType);
+
+      userEvent.click(loginButton);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('Wrong email or password')).toBeInTheDocument();
+    });
+  });
+
+  it('render success alert', async () => {
+    const emailInput = screen.getByText('Email');
+    const passwordInput = screen.getByText('Password');
+    const loginButton = screen.getByTestId('login-button');
+
+    const emailType = 'correct@mail.com';
+    const passwordType = '12345678';
+
+    act(() => {
+      userEvent.type(emailInput, emailType);
+      userEvent.type(passwordInput, passwordType);
+
+      mockFetchAPI(emailType, passwordType);
 
       userEvent.click(loginButton);
     });
