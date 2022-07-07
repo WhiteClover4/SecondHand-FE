c6ca6334763aebef8d1e82b66f3ffcd706c8ad13
Syahriful <dynes2014@gmail.com>
2022-07-07 20:53:42 +0700

    responsive sold page

--- src/pages/seller/Sold.js
+++ src/pages/seller/Sold.js
@@ -1,13 +1,14 @@
-import { MainNavbar } from '../../components/navbars';
-import { ProductCard } from '../../components/cards';
+import { SimpleNavbar } from '../../components/navbars';
+// import { ProductCard } from '../../components/cards';
 import { SellerLayout } from '../../components/layouts';
 
 export default function Sold() {
   return (
     <>
-      <MainNavbar />
+      <SimpleNavbar />
       <SellerLayout active={3}>
         <div className="grid grid-cols-3 gap-6">
+          {/* <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
@@ -15,8 +16,7 @@ export default function Sold() {
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard /> */}
         </div>
       </SellerLayout>
     </>
