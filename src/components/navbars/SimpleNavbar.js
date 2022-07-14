ab2853c9131a7ea4f93d810a6b04a0cb79e5971d
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-07-15 02:49:50 +0700

    hide back button

--- src/components/navbars/SimpleNavbar.js
+++ src/components/navbars/SimpleNavbar.js
@@ -5,7 +5,7 @@ export default function SimpleNavbar({ title }) {
   return (
     <header className="sticky top-0 z-10 flex h-[52px] w-full items-center bg-neutral-01 py-[14px] px-4 after:w-6 lg:h-[84px] lg:py-0 lg:px-[136px] lg:shadow-high lg:after:w-[100px]">
       <div className="w-6 lg:w-[100px]">
-        <BackButton />
+        <BackButton className="lg:hidden" />
         <Link className="hidden lg:inline" to="/">
           <div className="h-[34px] w-[100px] bg-primary-05"></div>
         </Link>
